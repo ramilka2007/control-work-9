739d4690c407489baa2914e538b7aaac14032b51
ramilka <lilya1976.03.03@gmail.com>
2024-07-27 13:15:04 +0600

    Сделал создание категории.

--- src/containers/Categories/Categories.tsx
+++ src/containers/Categories/Categories.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
+import AddCategory from "../AddCategory/AddCategory";
 
 const Categories = () => {
+    const [showAddModal, setShowAddModal] = React.useState(false);
     return (
         <>
             <div className="d-flex justify-content-between">
                 <h2>Categories</h2>
-                <button className="btn btn-primary">Add</button>
+                <button className="btn btn-primary" onClick={() => setShowAddModal(true)}>Add</button>
             </div>
-            <div>
-
-            </div>
+            <AddCategory show={showAddModal} onClose={() => setShowAddModal(false)}/>
         </>
     );
 };
